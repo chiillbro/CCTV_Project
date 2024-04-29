fa8a745c0ef081aff8cbd781e6eb9e2ff12111fb
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-04-29 12:35:22 +0530

    structured files

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+import database from "../assets/database.png";
+
+import AboutLogo from "../assets/AboutLogo.jpg";
+
+import AtticaLogo from "../assets/attica-gold-company-logo.jpg";
+
+import documentation from "../assets/documentation.png";
+
+export const logos = {
+  database,
+  aboutLogo: AboutLogo,
+  atticaLogo: AtticaLogo,
+  documentation,
+};
