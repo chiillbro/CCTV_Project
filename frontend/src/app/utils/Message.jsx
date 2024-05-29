e791b3898a7f156b2c510c1bff095636d134e4ac
Siva Sankar Reddy <aasamsivasankarreddynani143@gmail.com>
2024-05-29 14:05:45 +0530

    changes

--- frontend/src/app/utils/Message.jsx
+++ frontend/src/app/utils/Message.jsx
@@ -1,19 +1,16 @@
-
-const Message = ({ varient, children }) => {
-  const getVarientClass = () => {
-    switch (varient) {
-      case 'success':
-        return 'bg-green-100 text-green-800';
-      case 'error':
-        return 'bg-red-100 text-red-800';
+const Message = ({ variant, children }) => {
+  const getVariantClass = () => {
+    switch (variant) {
+      case "success":
+        return "bg-green-100 text-green-800";
+      case "error":
+        return "bg-red-100 text-red-800";
       default:
-        return 'bg-blue-100 text-blue-800';
+        return "bg-blue-100 text-blue-800";
     }
-  }
+  };
 
-  return (
-    <div className={`p-4 rounded ${getVarientClass}`}>{children}</div>
-  )
-}
+  return <div className={`p-4 rounded ${getVariantClass}`}>{children}</div>;
+};
 
-export default Message
+export default Message;
